a725b8fe375dcba704bc90f5abcfedc8a6eb9bc9
Ubuntu <vagrant@Base-Box>
2018-02-15 17:43:44 +0000

    Ran db migrate for new University model.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180215174103) do
+ActiveRecord::Schema.define(version: 20180215174238) do
 
   create_table "airports", force: :cascade do |t|
     t.string "code"
@@ -29,6 +29,13 @@ ActiveRecord::Schema.define(version: 20180215174103) do
   end
 
   create_table "crops", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "universities", force: :cascade do |t|
+    t.string "code"
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
