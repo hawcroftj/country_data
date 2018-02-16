37e82b6d6dd7c5cb00184a124a68b388727dec8c
Ubuntu <vagrant@Base-Box>
2018-02-16 17:15:52 +0000

    Added simple search functionality to all model pages.

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,7 +1,16 @@
 class SearchController < ApplicationController
   def index
     @terms = params[:q]
-
     @countries = Country.where("name LIKE (?)", "%#{@terms}%")
+  end
+
+  def airports
+    @terms = params[:q]
+    @airports = Airport.where("name LIKE (?)", "%#{@terms}%")
+  end
+  
+  def universities
+    @terms = params[:q]
+    @universities = University.where("name LIKE (?)", "%#{@terms}%")
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  get 'search', to: 'search#index', as: 'search'
+  get 'search_index', to: 'search#index', as: 'search_index'
+  get 'search_airports', to: 'search#airports', as: 'search_airports'
+  get 'search_universities', to: 'search#universities', as: 'search_universities'
 
   root to: 'countries#index'
   get 'pages/about' => 'pages#about'
