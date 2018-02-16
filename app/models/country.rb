a00d4b9027fcbc44cc3b7a3a0ac77b1a32387f77
Ubuntu <vagrant@Base-Box>
2018-02-16 19:22:55 +0000

    Added new erd document to data_files folder. Added areas to Country and Crop index/show pages to display associated objects.

--- app/models/country.rb
+++ app/models/country.rb
@@ -2,5 +2,6 @@ class Country < ApplicationRecord
   validates :code, :name, presence: true
   has_many :airports, :dependent => :destroy
   has_many :universities, :dependent => :destroy
+  has_many :country_crops
   has_many :crops, through: :country_crops
 end
--- app/models/crop.rb
+++ app/models/crop.rb
@@ -1,4 +1,5 @@
 class Crop < ApplicationRecord
   validates :name, presence: true
+  has_many :country_crops
   has_many :countries, through: :country_crops
 end
